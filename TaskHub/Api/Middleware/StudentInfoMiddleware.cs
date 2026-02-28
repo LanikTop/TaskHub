93977adccba1418af5de94ca2af1ee471f07d890
ruslan <russaf05@gmail.com>
2026-02-28 20:54:31 +0500

    feat: create StudentInfo and ResponseTime Middlewares

---
+++ TaskHub/Api/Middleware/StudentInfoMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace Api.Middleware
+{
+    public class StudentInfoMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public StudentInfoMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Response.Headers["X-Student-Name"] = "Safiullin Ruslan Ilfatovich";
+            context.Response.Headers["X-Student-Group"] = "PA-01";
+
+            await _next(context);
+        }
+    }
+}
