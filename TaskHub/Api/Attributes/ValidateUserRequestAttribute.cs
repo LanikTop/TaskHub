d82f40fe9123f978a3ba2253f703ee8b11d436c1
ruslan <russaf05@gmail.com>
2026-03-16 14:29:20 +0500

    Создал ValidateUserRequestAttribute

---
+++ TaskHub/Api/Attributes/ValidateUserRequestAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Reflection;
+
+namespace Api.Attributes
+{
+    public class ValidateUserRequestAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ActionArguments.TryGetValue("request", out var requestValue) || requestValue == null)
+            {
+                context.Result = new BadRequestObjectResult("Тело запроса отсутствует");
+                return;
+            }
+
+            var nameProperty = requestValue.GetType().GetProperty("name", BindingFlags.IgnoreCase);
+
+            if (nameProperty == null)
+            {
+                context.Result = new BadRequestObjectResult("Имя пользователя не задано");
+                return;
+            }
+
+            var nameValue = nameProperty.GetValue(requestValue) as string;
+
+            if (string.IsNullOrWhiteSpace(nameValue))
+            {
+                context.Result = new BadRequestObjectResult("Имя пользователя не задано");
+                return;
+            }
+        }
+    }
+}
