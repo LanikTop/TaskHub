cac4f0742952366db21a5bf2a4a13009eda1666c
ruslan <russaf05@gmail.com>
2026-03-16 14:19:04 +0500

    Создал ResponseTimeHeaderAttribute

---
+++ TaskHub/Api/Attributes/ResponseTimeHeaderAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace Api.Attributes
+{
+    public class ResponseTimeHeaderAttribute : ActionFilterAttribute
+    {
+        private Stopwatch _stopwatch;
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            _stopwatch.Stop();
+
+            context.HttpContext.Response.OnStarting(() =>
+            {
+                context.HttpContext.Response.Headers["X-Response-Time-Ms"] =
+                    _stopwatch.ElapsedMilliseconds.ToString();
+                return Task.CompletedTask;
+            });
+        }
+    }
+}
