724636a1c65b596eb53ad3529a01a27e34008fab
ruslan <russaf05@gmail.com>
2026-03-22 20:32:21 +0500

    Сделал шаблоны запросов

---
+++ TaskHub/Api/Controllers/Tasks/Request/CreateTaskRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Controllers.Tasks.Request
+{
+    public class CreateTaskRequest
+    {
+        public string? Title { get; set; }
+        public Guid CreatedByUserId { get; set; }
+    }
+}
