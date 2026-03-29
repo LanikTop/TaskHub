1a97fd9ce917e69e64bfff13e24e0d3632425742
ruslan <russaf05@gmail.com>
2026-03-29 16:56:21 +0500

    Создал StudentInfoHeadersFilter

---
+++ TaskHub/Api/Filters/StudentInfoHeadersFitler.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Filters
+{
+    public class StudentInfoHeadersFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.HttpContext.Response.Headers.Append("X-Student-Name", "Safiullin Ruslan Ilfatovich");
+            context.HttpContext.Response.Headers.Append("X-Student-Group", "PA-01");
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+    }
+}
