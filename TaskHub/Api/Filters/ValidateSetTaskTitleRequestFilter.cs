56e653153f2c341ec4ae9b4b95445ab91f7e1bc7
ruslan <russaf05@gmail.com>
2026-03-29 16:57:03 +0500

    Создал ValidateSetTaskTitleRequestFilter

---
+++ TaskHub/Api/Filters/ValidateSetTaskTitleRequestFilter.cs
@@ -0,0 +1,27 @@
+﻿using Api.Controllers.Tasks.Request;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Filters
+{
+    public class ValidateSetTaskTitleRequestFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ActionArguments.ContainsKey("request") ||
+                context.ActionArguments["request"] == null)
+            {
+                context.Result = new BadRequestObjectResult(new
+                {
+                    error = "Тело запроса отсутствует"
+                });
+                return;
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+    }
+}
