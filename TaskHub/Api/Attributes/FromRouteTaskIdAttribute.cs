3e05cfc644f0d1ccafe54a6447f01df3743fb74f
ruslan <russaf05@gmail.com>
2026-03-29 15:35:55 +0500

    Создал FromRouteTaskIdAttribute, который работает через ModelBinder

---
+++ TaskHub/Api/Attributes/FromRouteTaskIdAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Api.ModelBinders;
+
+namespace Api.Attributes
+{
+    public class FromRouteTaskIdAttribute : ModelBinderAttribute
+    {
+        public FromRouteTaskIdAttribute()
+        {
+            BinderType = typeof(TaskIdModelBinder);
+            BindingSource = BindingSource.Path;
+        }
+    }
+}
