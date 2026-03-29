8621fc36be8f30171885f550545dae31ca17a06a
ruslan <russaf05@gmail.com>
2026-03-29 15:36:17 +0500

    Создал TaskIdModelBinder

---
+++ TaskHub/Api/ModelBinders/TaskIdModelBinder.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Api.ModelBinders
+{
+    public class TaskIdModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+                throw new ArgumentNullException(nameof(bindingContext));
+
+            var valueProviderResult = bindingContext.ValueProvider.GetValue("id");
+            var routeValue = valueProviderResult.FirstValue;
+
+            if (string.IsNullOrEmpty(routeValue))
+            {
+                bindingContext.ModelState.AddModelError(
+                    bindingContext.ModelName,
+                    "Идентификатор задачи не задан");
+                return Task.CompletedTask;
+            }
+
+            if (!Guid.TryParse(routeValue, out var guidValue))
+            {
+                bindingContext.ModelState.AddModelError(
+                    bindingContext.ModelName,
+                    "Идентификатор задачи имеет некорректный формат");
+                return Task.CompletedTask;
+            }
+
+            bindingContext.Result = ModelBindingResult.Success(guidValue);
+            return Task.CompletedTask;
+        }
+    }
+}
